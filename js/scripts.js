6873c90d6af55736a5388363625eebdd0a096c24
Shawn <smithautoguy123@gmail.com>
2017-11-01 20:51:38 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+// var message = "Hello World";
+// console.log(typeof message);
+// var john = 23;
+// console.log(typeof john);
+// var bool = true;
+//
+// function practice(){
+// notDeclared = 'When it works how i expect it to...';
+//
+// var declared = "js is awesome!!";
+//
+// };
+//
+// console.log(declared, notDeclared);
+//var fresh = true, otherVar = 0;
+//
+// var message = "Hello World";
+//
+// var declared = "my variable"
+//
+// console.log(declared, notDeclared)
+
+var fresh = true, otherVar = 0;
+
+let computer = 'mac';
+
+if(fresh) {
+  let computer = 'mac';
+  console.log(computer);
+};
+
+var octance = true, otherVar = 0;
+
+if(fresh) {
+  let computer = 'broheem';
+  console.log(computer);
+};
+console.log(computer);
+
+
+if(fresh) {
+  let computer = 'macdaddy';
+  console.log(computer);
+}
+ console.log(computer);
+
+
+ const name = "ryan";
+ console.log(name);
+ if(fresh) {
+
+ }
+
+var(scythe) = true, otherVar = 0;
+
+if(scythe) {
+  var computer = 'scythe';
+  console.log(computer);
+};
+
+console.log(computer);
+
+var rightNow = new Date('5.21.86');
+console.log(rightNow);
+
+
+var name = 'mario';
+
+var stringObj = new String('John');
+
+console.log(stringObj.length);
+
+console.log(John.length);
